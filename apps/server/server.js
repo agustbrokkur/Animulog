3ef0a71c6f9b7bbd5584fc59b83dabf16fb2e492
Brokkur <ahagbgaha@gmail.com>
2026-04-30 23:23:10 +0200

    Added data saving functionality to the server

--- apps/server/server.js
+++ apps/server/server.js
@@ -11,6 +11,10 @@ function readData() {
     return JSON.parse(fs.readFileSync(DATA_FILE, "utf-8"));
 }
 
+function writeData(data) {
+    fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2), "utf-8");
+}
+
 app.get("/", (req, res) => {
     res.send("Animulog Server");
 });
@@ -18,6 +22,17 @@ app.get("/", (req, res) => {
 app.get("/api/data", (req, res) => {
     const data = readData();
     res.json(data);
+});
+
+app.post("/api/data", (req, res) => {
+    try {
+        writeData(req.body);
+        res.status(200)
+            .json({ ok: true });
+    } catch (error) {
+        res.status(500)
+            .json({ error: error.message })
+    }
 });
 
 app.listen(PORT, () => console.log(`Animulog Server running on http://localhost:${PORT}`));
