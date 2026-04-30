65e1d12485a9454b356cd61a85e67d1ee7b85c2d
Brokkur <ahagbgaha@gmail.com>
2026-04-30 22:47:40 +0200

    Added basic json reading and sending capability to the server

--- apps/server/server.js
+++ apps/server/server.js
@@ -1,9 +1,23 @@
 import express from "express";
-const app = express();
-const PORT = 3001;
+import fs from "fs";
 
-app.listen(PORT, () => console.log(`Animulog Server running on http://localhost:${PORT}`));
+const PORT = 3001;
+const DATA_FILE = "./data/data.json";
+
+const app = express();
+app.use(express.json({ limit: '10mb' }));
+
+function readData() {
+    return JSON.parse(fs.readFileSync(DATA_FILE, "utf-8"));
+}
 
 app.get("/", (req, res) => {
     res.send("Animulog Server");
 });
+
+app.get("/api/data", (req, res) => {
+    const data = readData();
+    res.json(data);
+});
+
+app.listen(PORT, () => console.log(`Animulog Server running on http://localhost:${PORT}`));
